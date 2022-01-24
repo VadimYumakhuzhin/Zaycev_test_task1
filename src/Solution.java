998d393790b586c62313ddfd4c11b98562754491
unknown <yumakhuzhinv@mail.ru>
2022-01-24 18:46:53 +0500

    First commit

---
+++ src/Solution.java
@@ -0,0 +1,51 @@
+/**
+ * Задача 1.
+ * Есть кусочек кода. Его создатель хотел получить следующий вывод в консоль:
+ * 7
+ * Developer
+ * Почему данная реализация не обеспечивает желаемого?
+ * Доработайте код.
+ * public static void main(String[] args) {
+ *      Employee tester = new Employee(7,“Developer”);
+ *      System.out.println(tester.getId());
+ *      System.out.println(tester.getVacancy());
+ * }
+ *  class Employee {
+ *      private long id;
+ *      private Vacancy vacancy;
+ *      String getVacancy() {
+ *          return vacancy.getVacancy();
+ *      }
+ *      long getId() {
+ *          return id;
+ *      }
+ *  }
+ *  class Vacancy {
+ *      String getVacancy() {
+ *          return "engineer";
+ *      }
+ *  }
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        Employee tester = new Employee(7,"Developer");
+        System.out.println(tester.getId());
+        System.out.println(tester.getVacancy());
+    }
+    class Employee {
+        private long id;
+        private Vacancy vacancy;
+        String getVacancy() {
+            return vacancy.getVacancy();
+        }
+        long getId() {
+            return id;
+        }
+    }
+    class Vacancy {
+        String getVacancy() {
+            return "engineer";
+        }
+    }
+}
