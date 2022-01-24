664587d097274f2ac660c3f665be7dc3fb7c1a3b
VadimYumakhuzhin <82927623+VadimYumakhuzhin@users.noreply.github.com>
2022-01-24 20:29:36 +0500

    Update Solution.java
    Adding indents between lines

--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,11 @@
 public class Solution {
+    
     public static void main(String[] args) {
         Employee tester = new Employee(7,"Developer");
         System.out.println(tester.getId());
         System.out.println(tester.getVacancy());
     }
+    
     static class Employee {
         private long id;
         private Vacancy vacancy;
@@ -21,6 +23,7 @@ public class Solution {
             return id;
         }
     }
+    
     static class Vacancy {
         private String vacancy;
 
